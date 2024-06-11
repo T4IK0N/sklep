a0cd53f85e496d8a7d747880fa5af5fd3f956486
T4IK0N <gasowskibogumil@gmail.com>
2024-06-11 14:05:41 +0200

    commit

--- scripts/koszyk.js
+++ scripts/koszyk.js
@@ -4,7 +4,16 @@ closeModal();
 
 // PRZEJDZ DO PLATNOSCI (zrobic)
 
-// function zjemCiMame() {
-//     zjemCiMame();
-// }
-// zjemCiMame();
+
+// FUNCTION FROM COMMON.JS
+
+function closeModalOnClickOutside(event) {
+    const modal = document.getElementById('cart-modal');
+    const cartIcon = document.getElementById('cart-icon-container');
+
+    if (modal.style.display !== "none" &&
+        !modal.contains(event.target) &&
+        !cartIcon.contains(event.target)) {
+        closeModal();
+    }
+}
