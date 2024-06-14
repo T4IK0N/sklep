fd8c0bf034bb49daf62747230feb8864603fbd43
T4IK0N <gasowskibogumil@gmail.com>
2024-06-14 12:56:01 +0200

    com

--- scripts/koszyk.js
+++ scripts/koszyk.js
@@ -1,22 +1,4 @@
-// CLOSE MODAL ON START
-
-closeModal();
-
 // PRZEJDZ DO PLATNOSCI (zrobic)
-
-
-// FUNCTION FROM COMMON.JS
-
-function closeModalOnClickOutside(event) {
-    const modal = document.getElementById('cart-modal');
-    const cartIcon = document.getElementById('cart-icon-container');
-
-    if (modal.style.display !== "none" &&
-        !modal.contains(event.target) &&
-        !cartIcon.contains(event.target)) {
-        closeModal();
-    }
-}
 
 
 function deleteProduct() {
@@ -26,13 +8,31 @@ function deleteProduct() {
 
 // pozycja ceny w koszyku (zeby zawsze byla na gorze i w jednej pozycji)
 
-// function f() {
+// let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
+
+// window.addEventListener('scroll', function() {
+
 //     const cart = document.getElementById('cart-price');
-//     const rect = cart.getBoundingClientRect();
+//     const cart_rect = cart.getBoundingClientRect();
+//     const products = document.getElementById('cart-products');
+//     const products_rect = products.getBoundingClientRect();
 
-//     setInterval(() => {
-//         console.log(rect.top);
-//     }, 500)
-// }
+//     const scrollTop = window.scrollY || window.pageYOffset;
+//     const scrollBottom = scrollTop + cart_rect.height;
 
-// window.addEventListener('resize', f());
+//     if (products_rect.top < 0) {
+//         if (products_rect.top <= 0) {
+//             cart.style.top = scrollTop + 'px';
+//         } else {
+//             cart.style.top = products_rect.top + 'px';
+//         }
+//     }
+    
+//     // if (cart_rect.y  products_rect.y) {
+//     //     cart.style.top = 0 + 'px';
+//     // }
+
+//     console.log("Najwyższy widoczny punkt:", scrollTop);
+//     console.log("Najniższy widoczny punkt:", scrollBottom);
+//     console.log("cart rect: ", cart_rect.top, cart_rect.bottom);
+// });
